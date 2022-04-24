0adf9786377aab61c08de1cfe003090a9ab63622
wairimu.Kanene <mercykanene254@gmail.com>
2022-04-24 13:36:44 +0300

    commit12-added TestUser that helps test cases for users

--- password_test.py
+++ password_test.py
@@ -1,3 +1,10 @@
 import unittest #unitest module
 from password import Credential #imports Credential class for testing
 from password import User #imports User class for testing
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class that helps define test cases for the credentials class behaviours
+    Args:
+        Testcase class taht helps create test cases for User
+    '''
