78c0bb49cab7d7d17e3d152803da27e986c4137f
wairimu.Kanene <mercykanene254@gmail.com>
2022-04-24 14:23:29 +0300

    commit25-run test for creating a new account and to save account

--- run.py
+++ run.py
@@ -14,3 +14,18 @@ def save_profile(profile):
     Function to save user profile
     '''
     profile.save_profile()
+
+    ## user class code above
+## credential class code below
+def create_account(accountName, accountUsername, accountPassword):
+    '''
+    Function to creates a new account
+    '''
+    new_account = Credential(accountName, accountUsername, accountPassword)
+    return new_account
+
+def save_account(account):
+    '''
+    Function to save account
+    '''
+    account.save_account()
