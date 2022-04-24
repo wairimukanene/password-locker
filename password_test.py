fa318d3afce4a9021de67f1a4fafea9516a8c439
wairimu.Kanene <mercykanene254@gmail.com>
2022-04-24 14:03:41 +0300

    commit21-added test case to check if one can find an account by username and display information

--- password_test.py
+++ password_test.py
@@ -87,4 +87,16 @@ class TestCredential(unittest.TestCase):
         self.new_account.delete_account() #deletes account object
         self.assertEqual(len(Credential.credentials_list),1)
 
+        # search account by username
+    def test_find_account_by_username(self):
+        '''
+        Test case to check if we can find an account by username and display information
+        '''
+        self.new_account.save_account()
+        test_account = Credential('instagram', 'wairimuKanene', 'wa.irim.u')
+        test_account.save_account()
+
+        found_account = Credential.find_by_accountUsername('wa.irim.u')
+        self.assertEqual(found_account.accountUsername,test_account.accountUsername)
+
 
