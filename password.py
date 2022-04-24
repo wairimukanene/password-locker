5f4866ffc190be20833eb1eef050904b7e9dc874
wairimu.Kanene <mercykanene254@gmail.com>
2022-04-24 12:46:54 +0300

    commit1-created class user to help user create new account

---
+++ password.py
@@ -0,0 +1,14 @@
+class User:
+    '''
+    This class helps generate instances for creating a new account on this application
+    '''
+    profile_list = [] # empty profile list
+    def __init__(self, userName, password):
+        '''
+        This function helps define properties to create a PASSWORD-MANAGER account.
+        Args: 
+            userName: user's username
+            password: user's password
+        '''
+        self.userName = userName
+        self.password = password
